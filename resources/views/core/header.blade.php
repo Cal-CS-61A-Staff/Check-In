ea5c704b682fb22a2965bfcd6ed01ada3a43cdc7
colinschoen <colin@colinschoen.com>
2015-06-14 21:30:00 -0700

    Move semantic to public folder

---
+++ resources/views/core/header.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
+
+  <title>CS61A - Check In</title>
+
+  <link rel="stylesheet" type="text/css" href="../dist/semantic.css">
+  <link rel="stylesheet" type="text/css" href="homepage.css">
+
+  <script src="http://cdnjs.cloudflare.com/ajax/libs/jquery/2.0.3/jquery.js"></script>
+  <script src="../dist/semantic.js"></script>
+  <script src="homepage.js"></script>
+
+</head>
+<body id="home">
