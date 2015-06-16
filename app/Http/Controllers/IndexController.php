6b5aa6ae390de2e8c0698ba581422ad766e71bb8
colinschoen <colin@colinschoen.com>
2015-06-16 00:32:07 -0700

    Finish form validation

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -1,6 +1,6 @@
 <?php namespace App\Http\Controllers;
 
-use Auth;
+use Auth, Request, Validator;
 use Illuminate\Routing\Controller;
 
 class IndexController extends Controller {
@@ -32,10 +32,32 @@ class IndexController extends Controller {
     {
         //Get all of our form input
         $input = Request::all();
-        die (var_dump($input));
         //Start our validator
         $validator = Validator::make([
-        ], [], []);
+            "name" => $input["inputName"],
+            "sid" => $input["inputSID"],
+            "email" => $input["inputEmail"],
+            "password" => $input["inputPassword"],
+        ], [
+            "name" => "required",
+            "sid" => "required|integer",
+            "email" => "required|email",
+            "password" => "required|min:8",
+        ], [
+            "name.required" => "Please enter your full first and last name.",
+            "sid.required" => "Please enter your numeric student ID. You can find it on your Cal One Card.",
+            "sid.integer" => "Your student ID should be numeric.",
+            "email.required" => "Please enter your email.",
+            "email.email" => "That does not appear to be a valid email address.",
+            "password.required" => "You must enter a password.",
+            "password.min" => "Your password must be at least 8 characters",
+        ]);
+
+        if ($validator->fails())
+        {
+            //We have issues. Redirect back to the form with all of the input except the password and include the error messages
+            return redirect()->back()->withErrors($validator->errors())->withInput(Request::except('inputPassword'));
+        }
     }
 
 
--- resources/views/registration.blade.php
+++ resources/views/registration.blade.php
@@ -1,22 +1,35 @@
 @include('core.header')
 
+@if (!empty($errors))
 <div class="row">
     <div class="col-lg-12">
-        <form class="form" data-toggle="validator" id="registrationForm" method="POST" action="{{ URL::route("doregistration") }}">
+        @foreach($errors->all() as $error)
+            <div class="alert alert-danger alert-dismissable">
+                <button type="button" class="close" data-dismiss="alert">×</button>
+                {{{ $error }}}
+            </div>
+        @endforeach
+    </div>
+</div>
+@endif
+<div class="row">
+    <div class="col-lg-12">
+        <form class="form" data-toggle="" id="registrationForm" method="POST" action="{{ URL::route("doregistration") }}">
+            <input type="hidden" name="_token" value="{{ csrf_token() }}" />
             <div class="form-group">
                 <label for="inputName">Name: </label>
                 <div class="help-block with-errors"></div>
-                <input class="form-control" type="text" id="inputName" name="inputName" placeholder="Ex: Colin Schoen" required />
+                <input class="form-control" type="text" id="inputName" name="inputName" value="{{{ Input::old('inputName') }}}" placeholder="Ex: Colin Schoen" required />
             </div>
             <div class="form-group">
                 <label for="inputSID">Student ID (SID): </label>
                 <div class="help-block with-errors"></div>
-                <input class="form-control" type="number" id="inputSID" name="inputSID" placeholder="Ex: 12345678 (Check your Cal 1 Card)" required />
+                <input class="form-control" type="number" id="inputSID" name="inputSID" value="{{{ Input::old('inputSID') }}}" placeholder="Ex: 12345678 (Check your Cal 1 Card)" required />
             </div>
             <div class="form-group">
                 <label for="inputEmail">Email (@berkeley.edu): </label>
                 <div class="help-block with-errors"></div>
-                <input class="form-control" type="email" id="inputEmail" name="inputEmail" placeholder="Ex: cschoen@berkeley.edu" required />
+                <input class="form-control" type="email" id="inputEmail" name="inputEmail" value="{{{ Input::old('inputEmail') }}}" placeholder="Ex: cschoen@berkeley.edu" required />
             </div>
             <div class="form-group">
                 <label for="inputPassword">Password: </label>
