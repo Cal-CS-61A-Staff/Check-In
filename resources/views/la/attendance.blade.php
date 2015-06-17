7830684a3ab265b4433fc7b0a483ad7e60ef7805
colinschoen <colin@colinschoen.com>
2015-06-17 04:06:24 -0700

    Make attendance table responsive

--- resources/views/la/attendance.blade.php
+++ resources/views/la/attendance.blade.php
@@ -7,19 +7,21 @@
 </div>
 <div class="row" style="margin-top: 20px;">
     <div class="col-lg-12">
-        <table class="table table-striped table-hover table-bordered table-responsive">
-            <tr><th>Type</th><th>Date</th><th>Start Time</th><th>GSI</th><th>Makeup</th><th>Logged at</th></tr>
-            @foreach($checkins as $checkin)
-                <tr>
-                    <td>{{{ $checkin->type->name }}}</td>
-                    <td>{{{ $checkin->date }}}</td>
-                    <td>{{{ $checkin->time }}}</td>
-                    <td><span class="label label-danger"><i class="fa fa-bookmark fa-fw"></i> {{{ $checkin->ta->name }}}</td>
-                    <td>@if ($checkin->makeup == 1) Yes @else No @endif</td>
-                    <td>{{{ $checkin->created_at }}}</td>
-                </tr>
-            @endforeach
-        </table>
+        <div class="table-responsive">
+            <table class="table table-striped table-hover table-bordered table-responsive">
+                <tr><th>Type</th><th>Date</th><th>Start Time</th><th>GSI</th><th>Makeup</th><th>Logged at</th></tr>
+                @foreach($checkins as $checkin)
+                    <tr>
+                        <td>{{{ $checkin->type->name }}}</td>
+                        <td>{{{ $checkin->date }}}</td>
+                        <td>{{{ $checkin->time }}}</td>
+                        <td><span class="label label-danger"><i class="fa fa-bookmark fa-fw"></i> {{{ $checkin->ta->name }}}</td>
+                        <td>@if ($checkin->makeup == 1) Yes @else No @endif</td>
+                        <td>{{{ $checkin->created_at }}}</td>
+                    </tr>
+                @endforeach
+            </table>
+        </div>
     </div>
 </div>
 
