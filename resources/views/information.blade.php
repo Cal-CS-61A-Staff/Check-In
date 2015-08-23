9f35085075514d5acd78be558f2c25bdd544e97a
Colin Schoen <cschoen@berkeley.edu>
2015-08-23 11:31:13 -0700

    Update piazza link for fa15.

--- resources/views/information.blade.php
+++ resources/views/information.blade.php
@@ -14,7 +14,7 @@
             <p>If you are a lab assistant please <a target="_blank" href="{{ route("registration") }}">register</a> an account prior to your first day assisting in
                 the labs. You will be required to check in <strong>(when you are physically lab assisting with a present GSI)</strong> using this system every time you assist in the labs, office hours and other course functions
                 in order to receive hours credit. Once you have registered an account you can <a target="_blank" href="{{ route("login") }}">login</a> and use the
-                <a target="_blank" href="{{ route("lacheckin") }}">check in</a> form as well as view your attendance history. Please direct any questions to <a target="_blank" href="https://piazza.com/class/i99jqe1hfat3nb">Piazza</a> and more urgent inquiries to cschoen [at] berkeley.edu</p>
+                <a target="_blank" href="{{ route("lacheckin") }}">check in</a> form as well as view your attendance history. Please direct any questions to <a target="_blank" href="https://piazza.com/class/idmea03k6vr19c">Piazza</a> and more urgent inquiries to cschoen [at] berkeley.edu</p>
             <hr />
             <p><small>Developed by Colin Schoen</small></p>
         </div>
