4683d464190f9eeda04d3410975c1401e4cd737e
Colin Schoen <cschoen@berkeley.edu>
2015-08-31 10:27:09 -0700

    And fix that error too.

--- resources/views/assignments.blade.php
+++ resources/views/assignments.blade.php
@@ -106,17 +106,18 @@
                             {{{ $assignment->sec->category->name }}}
                         </td>
                         <td>
-                            {{{ $assignment->location }}}
+                            {{{ $assignment->sec->location }}}
                         </td>
                         <td>
                             <span class="label label-danger"><i class="fa fa-bookmark fa-fw"></i> {{{ $assignment->sec->ta->name }}}</span>
-                            @if ($assignment->sec->ta2 != -1)
+                            @if ($assignment->sec->second_gsi != -1)
                                 <span class="label label-danger"><i class="fa fa-bookmark fa-fw"></i> {{{ $assignment->sec->ta2->name }}}</span>
                             @endif
                         </td>
                         <td>
                             {{{ App\Section::daysToString($assignment->sec) }}}
                         </td>
+
                         <td>
                             {{{ $assignment->sec->start_time }}}
                         </td>
