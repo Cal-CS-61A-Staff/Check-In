01d0ba4181dc76d9c47b558354be5f4fecae4f28
Colin Schoen <cschoen@berkeley.edu>
2016-09-12 21:22:26 -0700

    Add Sentry reporting to error handler

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -24,6 +24,9 @@ class Handler extends ExceptionHandler {
 	 */
 	public function report(Exception $e)
 	{
+        if ($this->shouldReport($e)) {
+            app('sentry')->captureException($e);
+        }
 		return parent::report($e);
 	}
 
