79072dcb814df7646cb04861e5b6a60f6e1bf24e
Colin Schoen <cschoen@berkeley.edu>
2017-09-27 15:48:21 -0700

    Use forceSchema
    Signed-off-by: Colin Schoen <cschoen@berkeley.edu>

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,7 +13,7 @@ class AppServiceProvider extends ServiceProvider {
 	public function boot()
 	{
 		if (env("FORCE_HTTPS", false)) {
-			URL::forceScheme('https');
+			URL::forceSchema('https');
 		}
 
 	}
