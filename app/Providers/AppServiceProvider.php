8977d0faceb14337d33c55b0586567ed05f3230b
Colin Schoen <cschoen@berkeley.edu>
2017-09-27 15:42:51 -0700

    Use forceScheme instead of forceSchema
    Signed-off-by: Colin Schoen <cschoen@berkeley.edu>

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,7 +13,7 @@ class AppServiceProvider extends ServiceProvider {
 	public function boot()
 	{
 		if (env("FORCE_HTTPS", false)) {
-			URL::forceSchema('https');
+			URL::forceScheme('https');
 		}
 
 	}
