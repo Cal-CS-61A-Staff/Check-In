6f9969aa25c335a3c8fc7c2439ffd3a1dfa8a437
Colin Schoen <cschoen@berkeley.edu>
2018-01-22 22:39:15 -0800

    Fix issue with edit check in modal (#138)
    Signed-off-by: Colin Schoen <cschoen@berkeley.edu>

--- app/Http/Controllers/TAController.php
+++ app/Http/Controllers/TAController.php
@@ -50,7 +50,9 @@ class TAController extends Controller {
 
     public function get_module_checkins() {
         $checkins = Checkin::with("ta")->with("type")->with("user")->orderBy("created_at", "ASC")->get();
-        return view("ta.modules.checkins")->with(["checkins" => $checkins]);
+        $types = Type::all();
+        $staff = User::where("access", ">", "0")->orderBy("name")->get();
+        return view("ta.modules.checkins")->with(["checkins" => $checkins, "staff" => $staff, "types" => $types]);
     }
 
     public function get_module_secretword() {
