005b8d25711904e31e638dbbb5f462a1439fe826
Colin Schoen <cschoen@berkeley.edu>
2018-01-25 19:57:43 -0800

    Add type to search for GSI
    Signed-off-by: Colin Schoen <cschoen@berkeley.edu>

--- resources/views/la/checkin.blade.php
+++ resources/views/la/checkin.blade.php
@@ -34,7 +34,8 @@
         <div class="row">
             <div class="col-lg-12">
                 <h3><small>GSI</small></h3>
-                <select class="form-control" name="inputGSI" id="inputGSI">
+                <select class="selectpicker form-control" name="inputGSI" id="inputGSI" data-show-subtext="true" data-live-search="true">
+                {{--<select class="form-control" name="inputGSI" id="inputGSI">--}}
                     @foreach ($tas as $ta)
                         <option value="{{{ $ta->id }}}">{{{ $ta->name }}}</option>
                     @endforeach
