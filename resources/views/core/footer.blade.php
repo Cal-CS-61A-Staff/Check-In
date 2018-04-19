aad3a7aeec9c9d24694ff1e7cdbfe2e85bd42d0a
Colin Schoen <cschoen@berkeley.edu>
2018-04-19 13:26:46 -0700

    Change jQuery CDN to CloudFlare to fix certificate validation issues
    Signed-off-by: Colin Schoen <cschoen@berkeley.edu>

--- resources/views/core/footer.blade.php
+++ resources/views/core/footer.blade.php
@@ -2,7 +2,7 @@
         <footer style="margin-top: 50px;">
         </footer>
     </body>
-    <script src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
     <script src="/js/typed.min.js"></script>
     <script src="/js/validator.js"></script>
