4bd2ce486d2901cf79b517d954b138c0d337f72b
Jerry Xu <jerryxu@berkeley.edu>
2022-09-02 06:01:25 +0000

    Upgrade, pin frontend libraries

--- resources/views/core/footer.blade.php
+++ resources/views/core/footer.blade.php
@@ -3,18 +3,18 @@
         </footer>
     </body>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.4/js/bootstrap.min.js"></script>
     <script src="/js/typed.min.js"></script>
     <script src="/js/validator.js"></script>
     <script src="/js/sweetalert.min.js"></script>
-    <script src="//cdn.datatables.net/1.10.7/js/jquery.dataTables.min.js"></script>
-    <script src="//cdn.datatables.net/plug-ins/1.10.7/integration/bootstrap/3/dataTables.bootstrap.js"></script>
-    <script src="https://code.highcharts.com/highcharts.js"></script>
-    <script src="/packages/pickadate/picker.js"></script>
-    <script src="/packages/pickadate/picker.date.js"></script>
-    <script src="/packages/pickadate/picker.time.js"></script>
+    <script src="https://cdn.datatables.net/1.10.7/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/plug-ins/1.10.7/integration/bootstrap/3/dataTables.bootstrap.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/highcharts/6.2.0/highcharts.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/pickadate.js/3.6.4/picker.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/pickadate.js/3.6.4/picker.date.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/pickadate.js/3.6.4/picker.time.js"></script>
     <script src="/packages/ckeditor/ckeditor.js"></script>
-    <script src="//cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.6.3/js/bootstrap-select.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.13.18/js/bootstrap-select.min.js"></script>
     <script async defer src="https://buttons.github.io/buttons.js"></script>
     <script>
         $(function() {
